ed1482f81683a398ff8933b936b969385a1e8073
Maxim Darré <maxim.darre@gmail.com>
2024-04-30 10:30:30 -0400

    test: add unit test for the contract query

--- src/contract.rs
+++ src/contract.rs
@@ -31,3 +31,53 @@ mod query {
         Ok(resp)
     }
 }
+
+// Execute is not implemented but provided for testing purposes
+#[allow(dead_code)]
+pub fn execute(_deps: DepsMut, _env: Env, _info: MessageInfo, _msg: Empty) -> StdResult<Response> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};
+
+    use super::*;
+
+    #[test]
+    fn greet_query() {
+        // Instantiate a mock app
+        // It represents the blockchain state
+        let mut app = App::default();
+
+        // Create the contract and store it on the app
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        // Instantiate the contract
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &Empty {},
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        // Query the contract
+        let resp: GreetResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::Greet {})
+            .unwrap();
+
+        assert_eq!(
+            resp,
+            GreetResp {
+                message: "Hello World".to_owned()
+            }
+        );
+    }
+}
--- src/msg.rs
+++ src/msg.rs
@@ -1,11 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct GreetResp {
     pub message: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum QueryMsg {
     Greet {},
 }
