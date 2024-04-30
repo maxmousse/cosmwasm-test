ed2ee938aafe818da93e8b3732f0eae067dc41cc
Maxim Darré <maxim.darre@gmail.com>
2024-04-30 15:57:48 -0400

    feat: clean project for assesment

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,4 @@
-use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-use error::ContractError;
-use msg::{ExecuteMsg, InstantiateMsg};
-
 mod contract;
 mod error;
 mod msg;
 mod state;
-
-#[entry_point]
-pub fn instantiate(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    msg: InstantiateMsg,
-) -> StdResult<Response> {
-    contract::instantiate(deps, env, info, msg)
-}
-
-#[entry_point]
-pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg) -> StdResult<Binary> {
-    contract::query(deps, env, msg)
-}
-
-#[entry_point]
-pub fn execute(
-    deps: DepsMut,
-    _env: Env,
-    info: MessageInfo,
-    msg: ExecuteMsg,
-) -> Result<Response, ContractError> {
-    contract::execute(deps, _env, info, msg)
-}
